532927ea0464bf31edd94e6f74c68ca09b7c1f08
itix-enoks <enoks@tutanota.com>
2025-03-14 22:33:48 +0100

    Update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -116,13 +116,3 @@
  '(cperl-array-face ((t nil)))
  '(cperl-hash-face ((t (:inherit default))))
  '(underline ((t nil))))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("f700bc979515153bef7a52ca46a62c0aa519950cc06d539df4f3d38828944a2c" "1ad12cda71588cc82e74f1cabeed99705c6a60d23ee1bb355c293ba9c000d4ac" "00d7122017db83578ef6fba39c131efdcb59910f0fac0defbe726da8072a0729" "36c5acdaf85dda0dad1dd3ad643aacd478fb967960ee1f83981d160c52b3c8ac" "3d9938bbef24ecee9f2632cb25339bf2312d062b398f0dfb99b918f8f11e11b1" "b9c002dc827fb75b825da3311935c9f505d48d7ee48f470f0aa7ac5d2a595ab2" "a3a71b922fb6cbf9283884ac8a9109935e04550bcc5d2a05414a58c52a8ffc47" default))
- '(package-selected-packages
-   '(base16-theme go-mode highlight-numbers rainbow-delimiters ivy vterm sudo-edit rainbow-mode nginx-mode multiple-cursors markdown-mode magit gdscript-mode ef-themes company)))
